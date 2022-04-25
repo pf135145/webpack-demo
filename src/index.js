bc6504278fcfb549f3e25abdbd1a0e394f69ee8c
pf135145 <962914907@qq.com>
2022-04-25 20:03:15 +0800

    feat 1st

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import _ from 'lodash';
+// import '../assets/style.css';
+
+function component() {
+  const element = document.createElement('div');
+
+  // lodash（目前通过一个 script 引入）对于执行这一行是必需的
+  element.innerHTML = 'index';
+
+  return element;
+}
+
+console.log(_.join([1,2,3]))
+
+document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const TerserPlugin = require("terser-webpack-plugin")
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    app: './src/index.js',
+    // home: './src/home.js',
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false
+      })
+    ],
+  },
+  externals: {
+    lodash: '_'
+  },
+  output: {
+    filename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'index',
+      template: 'assets/index.html',
+      inject: 'body',
+      filename: 'index.html',
+      // chunks: ['app'],
+    }),
+    new BundleAnalyzerPlugin()
+    // new HtmlWebpackPlugin({
+    //   title: 'home',
+    //   template: 'assets/index.html',
+    //   inject: 'body',
+    //   filename: 'home.html',
+    //   chunks: ['home'],
+    // }),
+  ],
+};
